0da5c93ae4ce8b2cb6ddd2e731b3c2fa6f7de0d8
Michael Krol <28386738+krolmic@users.noreply.github.com>
2019-06-02 17:11:25 +0200

    Make sure 'favorites' is not null:

--- lib/model/state.dart
+++ lib/model/state.dart
@@ -3,10 +3,12 @@ import 'package:firebase_auth/firebase_auth.dart';
 class StateModel {
   bool isLoading;
   FirebaseUser user;
-  List<String> favorites;
+  // Make sure 'favorites' is not null:
+  List<String> favorites = [];
 
   StateModel({
     this.isLoading = false,
     this.user,
   });
 }
+
